b12d71ec55994eb8f3deedd55f80d1dc6a15141a
stangkudut <149715132+stangkudut@users.noreply.github.com>
2024-04-15 05:43:46 +0700

    Update bbr.sh

--- limit/bbr.sh
+++ limit/bbr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Edition : Stable Edition V3.0
-# Auther  : RanTempest
+# Auther  : ST4NGKUDUT
 # (C) Copyright 2023
 # =========================================
 
@@ -18,7 +18,7 @@ clear
 #	 menu
 #else
 
-echo -e "Installing TCP BBR Mod By RanTempest STORE"
+echo -e "Installing TCP BBR Mod By ST4NGKUDUT STORE"
 echo -e "Please Wait BBR Installation Will Starting . . ."
 sleep 5
 clear
